b067ccf98a85de682c4f5e091e986d8efc67f2a8
abdoulrasheed <abdulrasheedibrahim47@gmail.com>
2019-09-01 02:05:03 +0100

    secure aws keys

--- app/static/app/js/graph.js
+++ app/static/app/js/graph.js
@@ -2,7 +2,7 @@ var ctx = document.getElementById('myChart').getContext('2d');
 var myChart = new Chart(ctx, {
     type: 'bar',
     data: {
-        labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
+        labels: ['Car', 'Napep', 'Trident'],
         datasets: [{
             label: '# of Votes',
             data: [12, 19, 3, 5, 2, 3],
